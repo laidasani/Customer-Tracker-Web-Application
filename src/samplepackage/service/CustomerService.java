39343f9870f0593d5a31fa3845d7de1435d474d1
Luv Aidasani <laidasani@gmail.com>
2019-09-07 16:04:05 -0700

    Uploaded project

---
+++ src/samplepackage/service/CustomerService.java
@@ -0,0 +1,19 @@
+package samplepackage.service;
+
+import java.util.List;
+
+import samplepackage.entity.Customer;
+
+public interface CustomerService {
+	
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+
+	public List<Customer> searchCustomers(String theSearchName);
+
+}
